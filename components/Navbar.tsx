960653d368c6bad16ea07cece8057be854330c41
fogfox <fogfox@pop-os.fritz.box>
2025-01-11 18:11:50 +0100

    Style navbar and add placeholder items

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import { FloatingNav } from './ui/floating-navbar'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='max-w-7xl w-full'>
+      <FloatingNav
+        navItems={[
+          { name: 'Home', link: '/' },
+          { name: 'Konzept', link: '/' },
+          { name: 'FAQ', link: '/' },
+          { name: 'Kontakt', link: '/' },
+          { name: 'Blog', link: '/blog' }
+        ]} />
+    </div>
   )
 }
 
