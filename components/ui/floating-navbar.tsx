d905c978b1dd25dbe3fef79660f2bae35a63e728
fogfox <fogfox@pop-os.fritz.box>
2025-01-11 20:44:57 +0100

    Create schema for Events Schedule and Availability model

--- components/ui/floating-navbar.tsx
+++ components/ui/floating-navbar.tsx
@@ -53,7 +53,7 @@ export const FloatingNav = ({
             key={`link=${idx}`}
             href={navItem.link}
             className={cn(
-              "relative dark:text-neutral-50 items-center  flex space-x-1 text-neutral-600 dark:hover:text-neutral-300 hover:text-neutral-500"
+              "relative text-neutral-50 items-center flex space-x-1 hover:text-purple hover:-translate-y-1 duration-500"
             )}
           >
             <span className="block sm:hidden">{navItem.icon}</span>
